8ea2049e2fa50dbb41f32e3e0429aa76020ee34d
Kanhaiya Kumar <rajanindian25@gmail.com>
2026-03-03 01:38:37 +0530

    routes and models are set

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const app = express();
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const path = require("path");
+const ownersRouter = require("./routes/ownersRouter");
+const productsRouter = require("./routes/productsRouter");
+const usersRouter = require("./routes/usersRouter");
 
-const app = express();
+const db = require("./config/mongoose-connection");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -12,9 +16,13 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.send("WELCOME!!");
-});
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+
+// app.get("/", (req, res) => {
+//     res.send("WELCOME!!");
+// });
 
 
 
